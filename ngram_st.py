2c99ad9c2cccb4f812c64279cd9188a4537a3380
Lars G Johnsen <yoonsen@gmail.com>
2020-10-21 01:14:11 +0200

    Update ngram_st.py

--- ngram_st.py
+++ ngram_st.py
@@ -101,8 +101,9 @@ st.line_chart(df)
 
 #st.line_chart(tot)
 
+antall = st.sidebar.number_input( "For sjekking av fordeling i bøker - jo fler jo lenger ventetid, forskjellige søk vil vanligvis gi nye bøker", 10)
+
 if st.button('Sjekk fordeling i bøker'):
-    antall = st.number_input( 'Antall bøker - jo fler jo lenger ventetid, forskjellige søk vil vanligvis gi nye bøker (trykk på +/- for starte nye søk', 10)    
     wordlist = allword
     urns = {w:nb.book_urn(words=[w], ddk = ddk, period = (period_slider[0], period_slider[1]), limit = antall) for w in wordlist}
     data = {w: nb.aggregate_urns(urns[w]) for w in wordlist}
